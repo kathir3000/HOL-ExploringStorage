4dcda9a0e430b26ca00fd6f8439498e0649643a0
Hernan Meydac Jean <hernan.meydac@southworks.net>
2012-03-30 18:58:10 -0300

    first commit

---
+++ Source/Ex4-WorkingWithDrives/end/PhotoAlbum/Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿namespace PhotoAlbum
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Web.UI.WebControls;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Diagnostics;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using Microsoft.WindowsAzure.StorageClient;
+
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected string CurrentPath
+        {
+            get
+            {
+                string path = Request.Params["path"];
+                if (String.IsNullOrEmpty(path))
+                {
+                    return Global.ImageStorePath;
+                }
+
+                return path;
+            }
+        }
+
+        protected void LinqDataSource1_ContextCreating(object sender, LinqDataSourceContextEventArgs e)
+        {
+            e.ObjectInstance = new PhotoAlbumDataSource(this.CurrentPath);
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            GridView1.Columns[GridView1.Columns.Count - 1].Visible = (this.CurrentPath != Global.ImageStorePath);
+
+            if (RoleEnvironment.IsAvailable)
+            {
+                MountedDrives.DataSource = from item in CloudDrive.GetMountedDrives()
+                                           select new
+                                           {
+                                               Name = item.Key + " => " + item.Value,
+                                               Value = item.Key
+                                           };
+
+                MountedDrives.DataBind();
+                MountedDrives.SelectedValue = this.CurrentPath;
+                SelectDrive.Visible = true;
+
+                NewDrive.Text = MountedDrives.Items.Count < 2 ? "New Drive" : "Delete Drive";
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Delete")
+            {
+                var index = Convert.ToInt32(e.CommandArgument);
+                string fileName = ((GridView)e.CommandSource).DataKeys[index].Value as string;
+                File.Delete(fileName);
+                SelectImageStore(CurrentPath);
+            }
+        }
+
+        protected void NewDrive_Click(object sender, EventArgs e)
+        {
+            if (RoleEnvironment.IsAvailable)
+            {
+                // retrieve storage account
+                CloudStorageAccount account = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+
+                // build page blob URI for the new cloud drive by changing the extension in the original URI
+                string imageStoreBlobUri = RoleEnvironment.GetConfigurationSettingValue("ImageStoreBlobUri");
+                string cloneStoreBlobUri = Path.ChangeExtension(imageStoreBlobUri, "bak");
+
+                // create drive and its associated page blob
+                CloudDrive clonedDrive = account.CreateCloudDrive(cloneStoreBlobUri);
+                if (MountedDrives.Items.Count < 2)
+                {
+                    try
+                    {
+                        clonedDrive.Create(16);
+                    }
+                    catch (CloudDriveException)
+                    {
+                        // cloud drive already exists
+                    }
+
+                    // mount the drive and retrieve its path
+                    LocalResource cache = RoleEnvironment.GetLocalResource("LocalDriveCache");
+                    string clonedStorePath = clonedDrive.Mount(cache.MaximumSizeInMegabytes / 2, DriveMountOptions.None);
+
+                    // copy the contents from the original drive to the new drive                
+                    foreach (string sourceFileName in Directory.GetFiles(Global.ImageStorePath, "*.*").Where(name => name.EndsWith(".jpg") || name.EndsWith(".png")))
+                    {
+                        string destinationFileName = Path.Combine(clonedStorePath, Path.GetFileName(sourceFileName));
+                        File.Copy(sourceFileName, destinationFileName, true);
+                    }
+
+                    SelectImageStore(clonedStorePath);
+                }
+                else
+                {
+                    clonedDrive.Unmount();
+                    clonedDrive.Delete();
+                    SelectImageStore(Global.ImageStorePath);
+                }
+            }
+        }
+
+        protected void MountedDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectImageStore(MountedDrives.SelectedValue);
+        }
+
+        private void SelectImageStore(string path)
+        {
+            Response.Redirect(this.Request.Path + "?path=" + path);
+        }
+    }
+}
---
+++ Source/Ex4-WorkingWithDrives/end/PhotoAlbum/Global.asax.cs
@@ -0,0 +1,92 @@
+﻿namespace PhotoAlbum
+{
+    using System;
+    using System.IO;
+    using System.Web.Configuration;
+    using System.Diagnostics;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using Microsoft.WindowsAzure.StorageClient;
+
+    public class Global : System.Web.HttpApplication
+    {
+        private static string imageStorePath;
+
+        // Application_Start is called after the OnStart method.
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            if (imageStorePath == null)
+            {
+                ImageStorePath = WebConfigurationManager.AppSettings["ImageStorePath"];
+            }
+
+            // initialize storage account configuration setting publisher
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
+            {
+                string connectionString = RoleEnvironment.GetConfigurationSettingValue(configName);
+                configSetter(connectionString);
+            });
+
+            try
+            {
+                // initialize the local cache for the Azure drive
+                LocalResource cache = RoleEnvironment.GetLocalResource("LocalDriveCache");
+                CloudDrive.InitializeCache(cache.RootPath + "cache", cache.MaximumSizeInMegabytes);
+
+                // retrieve storage account 
+                CloudStorageAccount account = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+
+                // retrieve URI for the page blob that contains the cloud drive from configuration settings 
+                string imageStoreBlobUri = RoleEnvironment.GetConfigurationSettingValue("ImageStoreBlobUri");
+
+                // unmount any previously mounted drive.
+                foreach (var drive in CloudDrive.GetMountedDrives())
+                {
+                    var mountedDrive = account.CreateCloudDrive(drive.Value.PathAndQuery);
+                    mountedDrive.Unmount();
+                }
+
+                // create the Windows Azure drive and its associated page blob
+                CloudDrive imageStoreDrive = account.CreateCloudDrive(imageStoreBlobUri);
+                try
+                {
+                    imageStoreDrive.Create(16);
+                }
+                catch (CloudDriveException)
+                {
+                    // drive already exists
+                }
+
+                // mount the drive and initialize the application with the path to the image store on the Azure drive
+                Global.ImageStorePath = imageStoreDrive.Mount(cache.MaximumSizeInMegabytes / 2, DriveMountOptions.None);
+            }
+            catch (CloudDriveException driveException)
+            {
+                Trace.WriteLine("Error: " + driveException.Message);
+            }
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            // obtain a reference to the cloud drive and unmount it
+            CloudStorageAccount account = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+            string imageStoreBlobUri = RoleEnvironment.GetConfigurationSettingValue("ImageStoreBlobUri");
+            CloudDrive imageStoreDrive = account.CreateCloudDrive(imageStoreBlobUri);
+            imageStoreDrive.Unmount();
+        }
+
+        public static string ImageStorePath
+        {
+            get
+            {
+                return imageStorePath;
+            }
+
+            set
+            {
+                imageStorePath = value;
+            }
+        }
+    }
+}
